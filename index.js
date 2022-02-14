5f0eeebda54732a60e4772638c42e885b84682d9
MarcusKruuleht <olen.marcus.lahe@gamil.com>
2022-02-14 09:31:13 +0200

    Added handlebars template engine

---
+++ index.js
@@ -0,0 +1,33 @@
+// application packages
+const express = require('express')
+const app = express()
+
+const path = require('path')
+// add template engine
+const hbs = require('express-handlebars');
+// setup template engine directory and files extension
+app.set('views', path.join(__dirname, 'views'));
+app.set('view-engine', 'hbs');
+app.engine('hbs', hbs.engine({
+    extname: 'hbs',
+    defaultLayout: 'main',
+    layoutsDir: __dirname + '/views/layouts',
+}))
+
+const mysql = require('mysql');
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended: true}))
+
+// create database connection
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "password",
+  database: "joga_mysql"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected to joga_mysql db");
+});
